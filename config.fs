a627cad161553e13ffc8cb0ea3741d486421ea33
Luca Stefani <luca020400@lineageos.org>
2017-08-28 16:05:06 +0200

    msm8937-common: Move to fs config gen
    Change-Id: Iacba15145adb4176ac8c88df3dd4b48d139f1365

---
+++ config.fs
@@ -0,0 +1,11 @@
+[system/vendor/bin/wcnss_filter]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: BLOCK_SUSPEND
+
+[system/vendor/bin/pm-service]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
