54b59308f4227250e086fbee8f94a75e93d02b12
Michael Bestas <mkbestas@lineageos.org>
2018-05-08 23:30:37 +0300

    data-ipa-cfg-mgr: Remove tests and unused files
    Change-Id: I627a36f0591f13f06b754e888dbf9fcd99ead1cb

--- data-ipa-cfg-mgr/ipacm/src/Android.mk
+++ data-ipa-cfg-mgr/ipacm/src/Android.mk
@@ -90,20 +90,6 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_EXECUTABLES)
 LOCAL_CLANG := true
 include $(BUILD_EXECUTABLE)
 
-################################################################################
-
-define ADD_TEST
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := $1
-LOCAL_SRC_FILES    := $1
-LOCAL_MODULE_CLASS := ipacm
-LOCAL_MODULE_TAGS  := debug
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
-include $(BUILD_PREBUILT)
-
-endef
-
 include $(CLEAR_VARS)
 LOCAL_MODULE := IPACM_cfg.xml
 LOCAL_MODULE_CLASS := ETC
