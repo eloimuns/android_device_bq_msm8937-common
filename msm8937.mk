5deb2958d676ad81ae2964c904e3cf2b792a0d48
Eloi Carracedo <eloimuns@gmail.com>
2018-09-03 17:26:10 +0200

    Changes 4 build
    Change-Id: Ia57f5055b8f2364232768ef1be2d801c7e7e02e8

--- msm8937.mk
+++ msm8937.mk
@@ -100,17 +100,18 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    copybit.msm8937 \
     gralloc.msm8937 \
     hwcomposer.msm8937 \
     memtrack.msm8937 \
-    liboverlay
+    liboverlay \
+    libdisplayconfig
 
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
     android.hardware.memtrack@1.0-impl
 
 # Data
